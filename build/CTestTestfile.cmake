72f447626ad95615e52d5d84b2bf5b495ef1549d
DTJB <63565569+my-Rx@users.noreply.github.com>
2024-02-28 20:45:38 +0900

    create project with cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Data/Source/Repos/my-Rx/studyC
+# Build directory: D:/Data/Source/Repos/my-Rx/studyC/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
