13479b3868e7f85ebc32892fbddc3378b51439db
DTJB <63565569+my-Rx@users.noreply.github.com>
2024-02-29 00:47:17 +0900

    Set CMakeLists compile Each Projects

---
+++ build/CMakeFiles/studyC.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Data/Source/Repos/my-Rx/studyC/main.c" "CMakeFiles/studyC.dir/main.c.obj" "gcc" "CMakeFiles/studyC.dir/main.c.obj.d"
+  "D:/Data/Source/Repos/my-Rx/studyC/src/Project/pro.c" "CMakeFiles/studyC.dir/src/Project/pro.c.obj" "gcc" "CMakeFiles/studyC.dir/src/Project/pro.c.obj.d"
+  "D:/Data/Source/Repos/my-Rx/studyC/src/Project1/pro1.c" "CMakeFiles/studyC.dir/src/Project1/pro1.c.obj" "gcc" "CMakeFiles/studyC.dir/src/Project1/pro1.c.obj.d"
+  "D:/Data/Source/Repos/my-Rx/studyC/src/Project2/pro2.c" "CMakeFiles/studyC.dir/src/Project2/pro2.c.obj" "gcc" "CMakeFiles/studyC.dir/src/Project2/pro2.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Project/projectN.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int, char**){
+    printf("Hello, from codeTest!\n");
+    printf("This is proj N\n");
+    getchar();
+}
